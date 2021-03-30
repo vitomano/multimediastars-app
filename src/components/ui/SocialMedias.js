e7d7e51247ccb838af1ed47d37e8934a36d26a57
Gustavo Hermosilla <vito.hermosilla@gmail.com>
2021-03-30 17:05:27 -0300

    new march

--- src/components/ui/SocialMedias.js
+++ src/components/ui/SocialMedias.js
@@ -10,7 +10,7 @@ export const SocialMedias = () => {
             <div className="container">
                 <img src={PhoneSocial} alt="phone-social-medias" />
                 <div className="px-5">
-                    <h2 className="primary mb-3">Social Medias<span className="secondary">.</span></h2>
+                    <h2 className="primary mb-3">Social Mediasss<span className="secondary">.</span></h2>
                     <p className="mb-3">It is a long established fact that a reader will be distracted by the readable content of a page. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters. 'Content here, content here', making it look like readable English.</p>
                     <Link to="/" className="btn">Read More</Link>
                 </div>
